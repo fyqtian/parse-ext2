68e1ed17a6369705e84b9c3e0cf4f7ac40192e60
fanv <van.fan@autodesk.com>
2025-01-05 13:37:31 +0800

    feat(init):

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # parse-ext2
 
-#### This repository is for learning and contains a simple implementation of an ext2 filesystem parser. 
+#### This repository is for learning and contains a simple implementation of an ext2 filesystem parser.
 
 ## Resources
+
 - [ext2 Filesystem Specification](https://www.nongnu.org/ext2-doc/ext2.html)
+- [blog](https://www.cnblogs.com/f66666/p/18353033)
--- src/ext2.rs
+++ src/ext2.rs
@@ -24,7 +24,51 @@ impl Ext2 {
             .div_ceil(super_block.s_blocks_per_group) as usize;
         println!("Block group count: {}", block_group_count);
         let group_desc = self.read_group_descriptor(block_group_count).unwrap();
-        println!("{:?}", group_desc);
+        for g in group_desc {
+            println!("{:?}", g);
+            let inode_offset = 4096 * g.bg_inode_table as u64;
+            self.fd.seek(SeekFrom::Start(inode_offset)).unwrap();
+            let mut buffer = [0u8; 128];
+            self.fd.read_exact(&mut buffer).unwrap();
+            let mut cursor = std::io::Cursor::new(buffer);
+            let inode = crate::layout::Inode {
+                mode: cursor.read_u16::<LittleEndian>().unwrap(),
+                uid: cursor.read_u16::<LittleEndian>().unwrap(),
+                size: cursor.read_u32::<LittleEndian>().unwrap(),
+                atime: cursor.read_u32::<LittleEndian>().unwrap(),
+                ctime: cursor.read_u32::<LittleEndian>().unwrap(),
+                mtime: cursor.read_u32::<LittleEndian>().unwrap(),
+                dtime: cursor.read_u32::<LittleEndian>().unwrap(),
+                gid: cursor.read_u16::<LittleEndian>().unwrap(),
+                links_count: cursor.read_u16::<LittleEndian>().unwrap(),
+                blocks: cursor.read_u32::<LittleEndian>().unwrap(),
+                flags: cursor.read_u32::<LittleEndian>().unwrap(),
+                osd1: cursor.read_u32::<LittleEndian>().unwrap(),
+                block: [
+                    cursor.read_u32::<LittleEndian>().unwrap(),
+                    cursor.read_u32::<LittleEndian>().unwrap(),
+                    cursor.read_u32::<LittleEndian>().unwrap(),
+                    cursor.read_u32::<LittleEndian>().unwrap(),
+                    cursor.read_u32::<LittleEndian>().unwrap(),
+                    cursor.read_u32::<LittleEndian>().unwrap(),
+                    cursor.read_u32::<LittleEndian>().unwrap(),
+                    cursor.read_u32::<LittleEndian>().unwrap(),
+                    cursor.read_u32::<LittleEndian>().unwrap(),
+                    cursor.read_u32::<LittleEndian>().unwrap(),
+                    cursor.read_u32::<LittleEndian>().unwrap(),
+                    cursor.read_u32::<LittleEndian>().unwrap(),
+                    cursor.read_u32::<LittleEndian>().unwrap(),
+                    cursor.read_u32::<LittleEndian>().unwrap(),
+                    cursor.read_u32::<LittleEndian>().unwrap(),
+                ],
+                generation: cursor.read_u32::<LittleEndian>().unwrap(),
+                file_acl: cursor.read_u32::<LittleEndian>().unwrap(),
+                dir_acl: cursor.read_u32::<LittleEndian>().unwrap(),
+                faddr: cursor.read_u32::<LittleEndian>().unwrap(),
+                osd2: [0; 12],
+            };
+            println!("{:?}", inode);
+        }
     }
 
     pub fn read_superblock(&mut self) -> io::Result<crate::layout::Ext2SuperBlock> {
